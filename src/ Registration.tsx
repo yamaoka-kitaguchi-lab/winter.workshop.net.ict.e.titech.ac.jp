26eafebe4d6451741e6f24cf35bea92a7820bff0
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-11-27 22:28:54 +0900

    Fix typo in registration page

--- src/ Registration.tsx
+++ src/ Registration.tsx
@@ -22,7 +22,7 @@ const Registration = () => {
 			<div>
 				<h3 className="mb-3">原稿投稿</h3>
 				<p>
-					A4ページ1枚分の原稿を作成し、以下のGoogleフォームから投稿してください．なお，フォーマットは，IEICE総合大会予稿に準じます．投稿期日は，
+					A4ページ1枚分の原稿を作成し、以下のGoogleフォームから投稿してください．なお，フォーマットは，IEICE総合大会予稿に準じます．投稿期日は
 					<mark className="yellow">12月14日（木）</mark>です．
 				</p>
 				<a
