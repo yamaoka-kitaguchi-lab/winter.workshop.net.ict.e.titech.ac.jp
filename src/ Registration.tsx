9ef217335e8e858ea70fcbc370e8e0e47596ea1c
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-11-28 02:47:16 +0000

    Update registration and submission deadlines

--- src/ Registration.tsx
+++ src/ Registration.tsx
@@ -6,7 +6,7 @@ const Registration = () => {
 				<h3 className="mb-3">参加登録</h3>
 				<p>
 					以下のGoogleフォームに「発表題目」「発表内容の要約（100字程度）」を登録してください。登録期日は
-					<mark className="yellow">12月11日（月）</mark>です。
+					<mark className="yellow">12月6日（水）</mark>です。
 				</p>
 				<a
 					className="btn btn-primary"
@@ -23,7 +23,7 @@ const Registration = () => {
 				<h3 className="mb-3">原稿投稿</h3>
 				<p>
 					A4ページ1枚分の原稿を作成し、以下のGoogleフォームから投稿してください．なお，フォーマットは，IEICE総合大会予稿に準じます．投稿期日は
-					<mark className="yellow">12月14日（木）</mark>です．
+					<mark className="yellow">12月12日（火）</mark>です．
 				</p>
 				<a
 					className="btn btn-primary"
